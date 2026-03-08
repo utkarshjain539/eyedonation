99b36ec09ab3226ebc5c009cb4ee988768ae1561
utkarshjain539 <44532668+utkarshjain539@users.noreply.github.com>
2026-03-08 12:51:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const ABTYP_HEADERS = {
   "api-Key": "ABTYP_API_SECRET_KEY_@ABTYP2023#@763^%ggjhg%",
   "Content-Type": "application/json"
 };
-
+ 
 const PHONE_NUMBER_ID = "1049088024951885";
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN; 
 const FIXED_RECIPIENT = "918488861504";
